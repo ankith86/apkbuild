c6d13767e0fbd1a9778f4416dff990d4eaf2069d
ankith86 <55974015+ankith86@users.noreply.github.com>
2020-02-17 17:34:46 +0530

    test6

--- apk_builder3.sh
+++ apk_builder3.sh
@@ -7,7 +7,7 @@ function apk_Builder(){
        
          echo " Working directory is $PWD "
 
-         npx react-native bundle --dev false --platform android --entry-file index.js --bundle-output /android/app/src/main/assets/index.android.bundle --assets-dest /android/app/src/main/res
+         npx react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res
 
          echo "Android directory "
          cd android/
